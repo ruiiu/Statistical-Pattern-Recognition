67ffd0be264e5a919b20f4ea1101081623ad9441
ruiiu <91287176+ruiiu@users.noreply.github.com>
2021-11-09 23:53:10 -0500

    Delete train_test_split.m

--- train_test_split.m
+++
@@ -1,9 +0,0 @@
-% the data has to be 3D
-% p is the percentage of training data to the whole dataset
-function [xtrain,xtest] = train_test_split(x,p)
-    [~,~,m3] = size(x);
-    rng('default');
-    index = randperm(m3);
-    xtrain = x(:,:,index(1:round(p*m3)));
-    xtest = x(:,:,index(round(p*m3)+1:end));
-end
